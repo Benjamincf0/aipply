0a77c4a1bb7811ba2086fbccc41ebbb098f3aef9
Ewen <ewengue1432@gmail.com>
2025-11-15 10:18:10 -0500

    added id to type

--- backend/types.ts
+++ backend/types.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const JobSchema = z.object({
+  id: z.string().optional(),
   title: z.string(),
   company: z.string(),
   source: z.string(),
@@ -9,4 +10,4 @@ export const JobSchema = z.object({
   description: z.string().optional(),
 });
 
-export type Job = z.infer<typeof JobSchema>;
+export type JobSchema = z.infer<typeof JobSchema>;
